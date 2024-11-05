6653b9642c113e1829d972957c6d57300fadb324
Avinash <112330806+Uavina@users.noreply.github.com>
2024-11-05 15:00:53 +0530

    re

---
+++ src/products.js
@@ -0,0 +1,74 @@
+import imageProduct1 from './assets/images/1.png'
+import imageProduct2 from './assets/images/2.png'
+import imageProduct3 from './assets/images/3.png'
+import imageProduct4 from './assets/images/4.png'
+import imageProduct5 from './assets/images/5.png'
+import imageProduct6 from './assets/images/6.png'
+import imageProduct7 from './assets/images/7.png'
+import imageProduct8 from './assets/images/8.png'
+export const products = [
+    {
+        id: 1,
+        name: 'Superman',
+        price: 101,
+        image: imageProduct1,
+        description: 'Superman, one of the most iconic superheroes, was created by Jerry Siegel and Joe Shuster in 1938. He is from the planet Krypton, sent to Earth as a baby, and raised by the Kents in Smallville, Kansas. His alter ego is Clark Kent, a mild-mannered reporter for the Daily Planet. Superman possesses incredible abilities like super strength, flight, x-ray vision, and invulnerability. His symbol, the "S" on his chest, stands for hope. He is often depicted as a symbol of truth, justice, and the American way. His archenemy is Lex Luthor, and he has a complicated relationship with Lois Lane.',
+        slug: 'hertfoid-upholstered-chair'
+    },
+    {
+        id: 2,
+        name: 'Iron man',
+        price: 151,
+        image: imageProduct2,
+        description: 'Iron Man, a Marvel superhero, was created by writer Stan Lee and artists Larry Lieber, Don Heck, and Jack Kirby in 1963. His real identity is Tony Stark, a genius billionaire, inventor, and CEO of Stark Industries. After a near-fatal injury, Tony builds a suit of armor to save his life and escape captivity, later refining it to fight as Iron Man. The suit grants him superhuman strength, flight, and advanced weaponry. Tony is known for his wit, charisma, and complexity, often dealing with personal struggles like guilt and responsibility. His key nemesis is the Mandarin, and he plays a pivotal role in the Avengers.',
+        slug: 'bingdon-pholstered-chair-swivel'
+    },
+    {
+        id: 3,
+        name: 'Hulk',
+        price: 181,
+        image: imageProduct3,
+        description: 'The Hulk, a Marvel superhero, was created by writer Stan Lee and artist Jack Kirby in 1962. His real identity is Dr. Bruce Banner, a brilliant scientist who, after being exposed to gamma radiation, transforms into the Hulk when angered or stressed. The Hulk possesses immense strength, invulnerability, and healing abilities, growing stronger the angrier he becomes. Unlike Banner calm, intellectual personality, the Hulk is often depicted as a force of raw, uncontrollable rage. This duality creates a constant internal conflict for Banner. The Hulk has fought both as a hero and a misunderstood monster, with notable enemies like the Abomination.',
+        slug: 'jeses-minimore-modern-style-etta'
+    },
+    {
+        id: 4,
+        name: 'Wolverine',
+        price: 201,
+        image: imageProduct4,
+        description: 'Wolverine, a popular Marvel superhero, was created by writer Len Wein and artists John Romita Sr. and Herb Trimpe, debuting in 1974. His real name is James "Logan" Howlett, and he is a mutant with powerful regenerative healing abilities, heightened senses, and retractable claws made of adamantium—a nearly indestructible metal bonded to his skeleton. Wolverine is known for his rugged, tough persona and a fierce sense of loyalty, though he often struggles with his violent past. A member of the X-Men, he is a skilled fighter with a mysterious and long-lived history. His primary adversaries include Sabretooth and other mutants.',
+        slug: 'eses-minimore-modern-style-zakari'
+    },
+    {
+        id: 5,
+        name: 'Thor',
+        price: 251,
+        image: imageProduct5,
+        description: 'Thor, a Marvel superhero, is based on the Norse god of thunder and was created by writer Stan Lee, scripter Larry Lieber, and artist Jack Kirby in 1962. Thor is the son of Odin, the All-Father of Asgard, and is known for wielding the enchanted hammer Mjolnir, which grants him the power of flight, weather manipulation, and immense strength. As the protector of both Asgard and Earth, Thor is a noble and courageous warrior. His alter ego, Dr. Donald Blake, was introduced in early comics but later phased out. Thor’s primary adversary is his adopted brother Loki, the god of mischief. He is also a key member of the Avengers.',
+        slug: 'corrigan-studio-tufed-upholstered-wide-winback-armchair'
+    },
+    {
+        id: 6,
+        name: 'Wonder woman',
+        price: 111,
+        image: imageProduct6,
+        description: 'Wonder Woman, one of DC Comics most iconic superheroes, was created by psychologist William Moulton Marston and artist H.G. Peter in 1941. Her real name is Diana, Princess of Themyscira, an Amazonian warrior blessed by the Greek gods. She possesses superhuman strength, speed, agility, and the ability to fly. Wonder Woman is known for her Lasso of Truth, indestructible bracelets, and a tiara that can be used as a projectile weapon. As a symbol of peace, justice, and equality, she fights for humanity while balancing her roles as both warrior and diplomat. Her primary adversaries include Ares, the god of war, and Cheetah. Wonder Woman is also a founding member of the Justice League.',
+        slug: 'corrigan-studio-273-wide-armchair'
+    },
+    {
+        id: 7,
+        name: 'Mr marvel',
+        price: 121,
+        image: imageProduct7,
+        description: 'Ms. Marvel (Kamala Khan): Kamala Khan, a Pakistani-American teenager, debuted in 2013 as the new Ms. Marvel. She is a superhero fan, particularly of Captain Marvel (Carol Danvers), and gains her powers after being exposed to Terrigen Mist. Kamala has the ability to stretch, enlarge, and morph her body, powers often referred to as "polymorphic" abilities. She is known for her optimism, determination, and her journey of self-discovery as a Muslim superhero balancing her personal life with her responsibilities',
+        slug: 'leston-wide-upholstered-fabric'
+    },
+    {
+        id: 8,
+        name: 'Spider-Man',
+        price: 220,
+        image: imageProduct8,
+        description: 'Spider-Man, one of Marvel’s most beloved superheroes, was created by writer Stan Lee and artist Steve Ditko in 1962. His real identity is Peter Parker, a teenager who gains superhuman abilities after being bitten by a radioactive spider. These powers include enhanced strength, agility, the ability to cling to walls, and a "spider-sense" that warns him of danger. Peter designs web-shooters that allow him to swing through the city and capture foes with webbing. Spider-Man is known for his iconic red-and-blue costume and his motto, "With great power comes great responsibility." He faces many iconic villains, including Green Goblin, Doctor Octopus, and Venom, while juggling his personal life as a high school student and freelance photographer for the Daily Bugle.',
+        slug: 'stephanny-275-wide-tufted-armchair'
+    },
+];
